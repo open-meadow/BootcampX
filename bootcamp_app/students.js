b2ec8e8eb72c0ba67aeb1bb775bb7d325992e1d8
open-meadow <rohan.varughese@protonmail.com>
2022-12-15 02:01:45 +0000

    Implement parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,13 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-  SELECT students.id, students.name, cohort_id, cohorts.name AS cohort_name
-  FROM students
-  JOIN cohorts ON (cohorts.id = students.cohort_id)
-  WHERE cohorts.name LIKE '%${process.argv[2]}%'
-  LIMIT ${process.argv[3] || 5};
-`)
+const queryString = `
+SELECT students.id, students.name, cohort_id, cohorts.name AS cohort_name
+FROM students
+JOIN cohorts ON (cohorts.id = students.cohort_id)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+
+const values = [`%${cohortName}%`, limit]
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(user);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,18 +7,28 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-  FROM teachers
-  JOIN assistance_requests ON teacher_id = teachers.id
-  JOIN students ON student_id = students.id
-  JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name LIKE '%${process.argv[2] || 'JUL02'}%'
-  ORDER BY teacher;
-`)
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE '%${process.argv[2] || 'JUL02'}%'
+ORDER BY teacher;
+`;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+
+const values = [`%${cohortName}%`, limit]
+
+pool.query(queryString, values)
 .then(res => {
   console.log(res.rows);
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
-  })
+  });
+})
+.catch(err => {
+  console.error('query error', err.stack);
 })
